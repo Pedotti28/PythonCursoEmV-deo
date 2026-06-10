e7ca0cabad0232af9e9ef154723ab47df7cdda64
Pedotti28 <davipedotti@gmail.com>
2026-06-10 12:28:51 +1200

    Add files via upload

--- Exercício089BoletimLIstasCompostas.py
+++ Exercício089BoletimLIstasCompostas.py
@@ -1,10 +1,25 @@
-lista = [[[]]]
-n = 0
+lista = []
+
 while True:
-    lista[n][n].append(str(input('Nome: ')))
-    lista[n][n][n].append(float(input('Nota 1: ')))
-    lista[n][n][n].append(float(input('Nota 2: ')))
+    nome = input('Nome: ')
+    nota_1 = float(input('Nota 1: '))
+    nota_2 = float(input('Nota 2: '))
+    media = (nota_1 + nota_2) / 2
+    lista.append([nome, [nota_1, nota_2], media])
 
+    prox = ' '
+    while prox not in 'SN':
+        prox = input('Quer continuar? [S/N] ').upper()
+    if prox in 'N':
+        break
 
-
-
+print('-=' * 40)
+print(f'{"No.":<4}{"Nome":<10}{"MEDIA":>8}')
+for i, a in enumerate(lista):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+print('-' * 35)
+opc = int(input('Mostrar notas de qual aluno? (999 Interrompe): '))
+if opc == 999:
+    print('FINALIZANDO...')
+if opc <= len(lista) - 1:
+    print(f'Notas de {lista[opc][0]} são {lista[opc][1]}')
